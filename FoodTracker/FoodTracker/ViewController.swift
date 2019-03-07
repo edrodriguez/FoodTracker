61133111ecac0051f2256d0e1a56e4508d4464b1
Eduardo Rodriguez Macrillante <ed.roma19@gmail.com>
2019-03-06 21:18:35 -0500

    Copying from Computer

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Eduardo Rodriguez on 7/16/18.
+//  Copyright © 2018 Eduardo Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+}
+
